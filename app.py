51a1e3dda6995290e326283d7dcaa076372c93d7
Meir Leviev <meirleviev@gmail.com>
2026-01-26 22:51:25 +0200

    level-1 is complete!
    Added a SpaceNetwork, 2 satellites and a packet.
    sent the packet from sat1 to sat2.

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ class Satellite(SpaceEntity):
 
     def receive_signal(self, packet: Packet):
         print( f"[{self.name}] Received: {packet}" )
+
+ship1 = SpaceNetwork(level=1)
+sat1 = Satellite("sat1", 100)
+sat2 = Satellite("sat2", 200)
+msg = Packet("Hello from sat1!", sat1, sat2)
+
+ship1.send(msg)
