dd6a2e1be2ac9667a5dfeaa7e6fea33c356e3baf
Meir Leviev <meirleviev@gmail.com>
2026-01-27 09:05:28 +0200

    level-3 is complete!
    I also changed the func name from transmission_attempt to attempt_transmission I realize the name was backwards

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from space_network_lib import *
 import time
+from safe_transmission import BrokenConnectionError
 
 class Satellite(SpaceEntity):
 
     def receive_signal(self, packet: Packet):
         print( f"[{self.name}] Received: {packet}" )
 
-ship1 = SpaceNetwork(level=2)
+ship1 = SpaceNetwork(level=3)
 sat1 = Satellite("sat1", 100)
 sat2 = Satellite("sat2", 200)
 msg = Packet("Hello from sat1!", sat1, sat2)
 
-def transmission_attempt(packet):
+def attempt_transmission(packet):
     while True:
         try:
             ship1.send(packet)
             break
+
         except TemporalInterferenceError:
             print("Interference, waiting...")
             time.sleep(2)
@@ -23,4 +25,16 @@ def transmission_attempt(packet):
         except DataCorruptedError:
             print("Data corrupted, retrying...")
 
-transmission_attempt(msg)
+        except LinkTerminatedError:
+            print("Link lost")
+            raise BrokenConnectionError
+
+        except OutOfRangeError:
+            print("Target out of range")
+            raise BrokenConnectionError
+
+
+try:
+    attempt_transmission(msg)
+except BrokenConnectionError:
+    print("Transmission failed")
