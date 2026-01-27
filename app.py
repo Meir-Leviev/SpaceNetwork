138d47d0cc4900d63217bb086654125d1c886273
Meir Leviev <meirleviev@gmail.com>
2026-01-27 22:38:33 +0200

    Added new func

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from space_network_lib import *
 import time
-from safe_transmission import BrokenConnectionError, RelayPacket, wrap_r_p
+from safe_transmission import BrokenConnectionError, RelayPacket, wrap_r_p, smart_send_packet
 
 class Satellite(SpaceEntity):
 
@@ -17,16 +17,18 @@ class Earth(SpaceEntity):
     def receive_signal(self, packet: Packet):
         pass
 
-ship1 = SpaceNetwork(level=5)
+ship1 = SpaceNetwork(level=1)
 earth = Earth("Earth",0)
-sat1 = Satellite("sat1", 100)
-sat2 = Satellite("sat2", 200)
-sat3 = Satellite("sat3", 300)
-sat4 = Satellite("sat4", 400)
-l_sat = [earth,sat1,sat2,sat3,sat4]
+sat1 = Satellite("sat1", 50)
+sat2 = Satellite("sat2", 100)
+sat3 = Satellite("sat3", 150)
+sat4 = Satellite("sat4", 200)
+sat5 = Satellite("sat5", 250)
+sat6 = Satellite("sat6", 300)
+l_sat = [earth,sat1,sat2,sat3,sat4, sat5]
 # msg = Packet("Hello from sat1!", sat1, sat2)
-p_final1 = Packet("Hello from Earth!", sat3, sat4)
-p=wrap_r_p(p_final1, l_sat)
+msg1 = Packet("Hello from Earth!", earth, sat6)
+packet1 = smart_send_packet(msg1, l_sat)
 
 
 def attempt_transmission(packet):
@@ -52,7 +54,7 @@ def attempt_transmission(packet):
 
 
 try:
-    attempt_transmission(p)
+    attempt_transmission(packet1)
 except BrokenConnectionError:
     print("Transmission failed")
 
--- safe_transmission.py
+++ safe_transmission.py
@@ -11,8 +11,24 @@ class RelayPacket(Packet):
     def __repr__(self):
         return f"RelayPacket(Relaying [{self.data}] to {self.receiver}from {self.sender})"
 
-def wrap_r_p(p_final, lst_sat):
+def wrap_r_p(p_final, lst):
+    lst_sat = lst[::-1]
     r_pac = p_final
     for i in range(len(lst_sat) -2, 0,-1):
         r_pac = RelayPacket(r_pac , lst_sat[i-1], lst_sat[i])
     return r_pac
+
+def smart_send_packet(packet, lst_sat: list):
+    sender = packet.sender
+    receiver = packet.receiver
+    new_lst = [receiver]
+    temp_receiver = receiver
+    i = lst_sat.index(sender)
+    while temp_receiver != sender:
+        if lst_sat[i].distance_from_earth + 150 >= temp_receiver.distance_from_earth:
+            new_lst.append(lst_sat[i])
+            temp_receiver = lst_sat[i]
+            i = lst_sat.index(sender)
+        else:
+            i += 1
+    return wrap_r_p(packet, new_lst)
